66a5b10bab8bc6b5a115f59f0d11dda058242ea2
Drew S. Ortega <DrewSOrtega@gmail.com>
2019-12-01 14:51:40 -0800

    finish day 1 part 2, extra fuel

--- day1/fuel.py
+++ day1/fuel.py
@@ -1,14 +1,27 @@
 import sys
 import math
 
-def main():
+def module_fuel():
     total = 0
     for line in sys.stdin:
         try:
             total += math.floor(int(line)/3)-2
         except:
             pass
-    print(total)
+    return total
+
+def extra_fuel(weight):
+    added_fuel = math.floor(int(weight)/3)-2
+    if(added_fuel <= 0):
+        return 0
+
+    return added_fuel + extra_fuel(added_fuel)
+
+def main():
+    weight = module_fuel()
+    weight += extra_fuel(weight)
+    print(weight)
+
 
 if __name__ == "__main__":
     main()
