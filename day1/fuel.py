12dac971f4c16d7acd4a6ecc1d75f1b011535ec1
Drew S. Ortega <DrewSOrtega@gmail.com>
2019-12-01 15:28:03 -0800

    get extra fuel per module instead of just total

--- day1/fuel.py
+++ day1/fuel.py
@@ -5,21 +5,21 @@ def module_fuel():
     total = 0
     for line in sys.stdin:
         try:
-            total += math.floor(int(line)/3)-2
+            fuel_weight = math.floor(int(line)/3)-2
+            fuel_weight += extra_fuel(fuel_weight)
+            total += fuel_weight
         except:
             pass
     return total
 
 def extra_fuel(weight):
-    added_fuel = math.floor(int(weight)/3)-2
+    added_fuel = math.floor(weight/3)-2
     if(added_fuel <= 0):
         return 0
-
     return added_fuel + extra_fuel(added_fuel)
 
 def main():
     weight = module_fuel()
-    weight += extra_fuel(weight)
     print(weight)
 
 
