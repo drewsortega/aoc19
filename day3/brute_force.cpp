f3ba1359ba5b8adfcb0aaa5efeb5ce838dc0432c
Drew S. Ortega <DrewSOrtega@gmail.com>
2019-12-11 23:55:08 -0800

    begin day 3

---
+++ day3/brute_force.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <bits/stdc++.h>
+#include <string>
+#include <stdio>
+
+using namespace std;
+
+class Segment {
+public:
+    int min;
+    int max;
+    int pos;
+};
+int main() {
+    ios_base::sync_with_stdio(false);    
+    cin.tie(NULL);
+
+    std::list<Segment> h_segs;
+    std::list<Segment> v_segs;
+    
+    string wire1;
+    cin >> wire1; 
+    cout << wire1;
+    string token;
+    do {
+	token = strtok(NULL, ',');
+	list.push_back(	
+    } while (token != NULL);
+}
